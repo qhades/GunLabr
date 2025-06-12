c1aa6eeb7c1827371dc40b9f3640db669b59b842
Noah <ngodquangha@gmail.com>
2025-06-12 07:27:21 +0700

    Add InstructionScene

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -5,13 +5,64 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+    #region Header OBJECT REFERENCES
+    [Space(10)]
+    [Header("OBJECT REFERENCES")]
+    #endregion
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject quitButton;
+    [SerializeField] private GameObject highScoreButton;
+    [SerializeField] private GameObject instructionButton; 
+    [SerializeField] private GameObject returnToMainMenuButton;
+    private bool isInstructionSceneLoaded = false;
+
     private void Start()
     {
         SceneManager.LoadScene("CharacterSelectorScene", LoadSceneMode.Additive);
+
+        returnToMainMenuButton.SetActive(false);
     }
 
     public void PlayGame()
     {
         SceneManager.LoadScene("MainGameScene");
+    }
+
+    public void LoadCharacterSelector()
+    {
+        returnToMainMenuButton.SetActive(false);
+
+        if (isInstructionSceneLoaded)
+        {
+            SceneManager.UnloadSceneAsync("InstructionScene");
+            isInstructionSceneLoaded = false;
+        }
+
+        playButton.SetActive(true);
+        quitButton.SetActive(true);
+        highScoreButton.SetActive(true);
+        instructionButton.SetActive(true);
+
+        SceneManager.LoadScene("CharacterSelectorScene", LoadSceneMode.Additive);
+    }
+
+    public void LoadInstruction()
+    {
+        playButton.SetActive(false);
+        quitButton.SetActive(false);
+        highScoreButton.SetActive(false);
+        instructionButton.SetActive(false);
+        isInstructionSceneLoaded = true;
+
+        SceneManager.UnloadSceneAsync("CharacterSelectorScene");
+
+        returnToMainMenuButton.SetActive(true);
+
+        SceneManager.LoadScene("InstructionScene", LoadSceneMode.Additive);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
